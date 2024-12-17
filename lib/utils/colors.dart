40ac7e10e4f53dcf34b68ac9110137d8fc3540a0
Ahmad Asghar <ahmadasghar1155@gmail.com>
2024-12-17 22:31:20 +0500

    Added utility files

---
+++ lib/utils/colors.dart
@@ -0,0 +1,30 @@
+import 'dart:ui';
+
+class XColors {
+  XColors._();
+
+  /// Common Colors
+  static const Color kWhite = Color(0xffFFFFFF);
+  static const Color kBlack = Color(0xff000000);
+  static const Color kGrey = Color(0xff9E9E9E);
+  static const Color kRed = Color(0xffF44336);
+  static const Color kBlue = Color(0xff2196F3);
+  static const Color kGreen = Color(0xff4CAF50);
+  static const Color kYellow = Color(0xffFFEB3B);
+  static const Color kOrange = Color(0xffFF9800);
+  static const Color kPurple = Color(0xff9C27B0);
+  static const Color kBrown = Color(0xff795548);
+
+  /// Additional useful colors
+  static const Color kLightBlue = Color(0xff03A9F4);
+  static const Color kLightGreen = Color(0xff8BC34A);
+  static const Color kDeepOrange = Color(0xffFF5722);
+  static const Color kDeepPurple = Color(0xff673AB7);
+  static const Color kCyan = Color(0xff00BCD4);
+  static const Color kPink = Color(0xffE91E63);
+  static const Color kTeal = Color(0xff009688);
+  static const Color kAmber = Color(0xffFFC107);
+  static const Color kLime = Color(0xffCDDC39);
+  static const Color kIndigo = Color(0xff3F51B5);
+
+}
