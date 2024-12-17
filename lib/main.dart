dc927e30581c4d999bedaa1dcaea7e3d6cc068d7
Ahmad Asghar <ahmadasghar1155@gmail.com>
2024-12-17 14:20:10 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:trendhive/screens/auth/login/login.dart';
+
+void main() {
+  runApp(
+    GetMaterialApp(
+      home: Login(),
+    ),
+  );
+}
